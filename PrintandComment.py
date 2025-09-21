4ec8a95cc3a732b0effc80fed10b6638a14655c0
GihuguKim <kimgyannag1@gmail.com>
2025-09-21 11:17:01 +0200

    Add files via upload

---
+++ PrintandComment.py
@@ -0,0 +1,16 @@
+#printing a greeting message
+print("Hello, welcome to the Python programming world!")
+# Introducing my self
+print("My name is Kim Gyanna")
+# I am learning Python
+print("I am learning Python programming language.")
+# I love coding
+print("I love coding and creating new projects.")
+# I am excited to learn more
+print("I am excited to learn more about Python and its applications.")
+# Thank you for reading my comments
+print("Thank you for reading my comments!")
+#printing a number
+print(42,30,100)
+#printing a text and a number
+print("The answer is", 42)
